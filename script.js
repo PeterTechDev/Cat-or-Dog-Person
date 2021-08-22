a6b6cb7c7be313d36dd896228dc67275909543c0
Peter Leite <peterleite.dev@gmail.com>
2021-08-22 08:37:01 -0300

    refatorando as funções para receberem parametros

--- script.js
+++ script.js
@@ -2,27 +2,60 @@
 const dog = document.querySelectorAll('.dog');
 const cat = document.querySelectorAll('.cat');
 
-//Captura os botooes
+//Captura os botoes
 const catBtn = document.getElementById('btn-cat');
 const dogBtn = document.getElementById('btn-dog');
+// const petBtn = document.getElementsByClassName('petBtn');
 
-
-
-const catDisplay =()=>{
-        cat.forEach(element => {
-            element.classList.toggle('hide')
+const check = (pet)=>{
+    if(!pet[0].classList.contains('hide')){
+        pet.forEach(element => {
+            element.classList.add('hide')
         });
-    }
-
-
-
-const dogDisplay =()=> {
-    dog.forEach(element => {
-        element.classList.toggle('hide')
-    });
+    } 
 }
 
-    
-    
-catBtn.addEventListener("click", catDisplay)
-dogBtn.addEventListener("click", dogDisplay)
+const petDisplay =(petDisplay, petHide)=>{
+    petDisplay.forEach(element => {
+        element.classList.remove('hide')
+    });
+    check(petHide)
+}
+
+
+catBtn.addEventListener("click", ()=>{petDisplay(cat, dog)})
+dogBtn.addEventListener("click", ()=>{petDisplay(dog, cat)})
+
+//Quando apertar o botao gato, add hide aos dogs
+
+// const check = ()=>{
+//     if(!dog[0].classList.contains('hide')){
+//         dogDisplay()
+//     } 
+// }
+// const check2 = ()=>{
+//     if(!cat[0].classList.contains('hide')){
+//         catDisplay()
+//     } 
+// }
+
+// const catDisplay =()=>{
+//         cat.forEach(element => {
+//             element.classList.toggle('hide')
+//         });
+//         check()
+//         // dogDisplay()
+//     }
+
+// const dogDisplay =()=> {
+//     dog.forEach(element => {
+//         element.classList.toggle('hide')
+//     });
+//     check2()
+// }
+
+
+
+
+// catBtn.addEventListener("click", catDisplay)
+// dogBtn.addEventListener("click", dogDisplay)
