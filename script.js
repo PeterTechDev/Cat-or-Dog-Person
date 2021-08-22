689d96c2c2306adc38ec7cca5f66edd5484ef4bd
Peter Leite <peterleite.dev@gmail.com>
2021-08-22 12:24:35 -0300

    continua estilização e substitui as imagens

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ const dogBtn = document.getElementById('btn-dog');
 // Função para verificar se Cat ou Dog já tem a classe hide. Para apenas um dos dois serem mostrados de cada vez
 //Obs.: O primeiro console log foi adc pois não consigo deixar sem nada ali
 const hideDisplay = (pet) => {
-    (pet[0].classList.contains('hide')) ? console.log('Todos os pets são anjos') : pet.forEach(element => {element.classList.add('hide')});
+    (pet[0].classList.contains('hide')) ? console.log('Todos os pets são anjos'): pet.forEach(element => { element.classList.add('hide') });
 }
 
 // Função para percorrer a lista e remover o classe que a deixa escondida e invocando a função da linha 11 para fazer a verificação
-const petDisplay =(petShow, petHide)=>{
+const petDisplay = (petShow, petHide) => {
     petShow.forEach(element => {
         element.classList.remove('hide')
     });
@@ -27,5 +27,5 @@ const petDisplay =(petShow, petHide)=>{
 
 // Adiciona o evento para invocar a função da linha 16 quando o user 'clicar' nos botoes capturados
 //Obs.: Foi criada uma função anônima para invocar a função pois não consegui add os parâmetros de outra forma
-catBtn.addEventListener("click", ()=>{petDisplay(cat, dog)});
-dogBtn.addEventListener("click", ()=>{petDisplay(dog, cat)});
+catBtn.addEventListener("click", () => { petDisplay(cat, dog) });
+dogBtn.addEventListener("click", () => { petDisplay(dog, cat) });
