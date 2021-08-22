459e9e1e0db6ed7f41727daf6ea023d75cae6dc3
Peter Leite <peterleite.dev@gmail.com>
2021-08-22 09:19:41 -0300

    add comentários no JS

--- script.js
+++ script.js
@@ -1,61 +1,26 @@
-//CAPTURA todos os elementos que tem a classe em uma lista
+//CAPTURA todos os elementos que tem a classe e os add em uma lista
 const dog = document.querySelectorAll('.dog');
 const cat = document.querySelectorAll('.cat');
 
 //Captura os botoes
 const catBtn = document.getElementById('btn-cat');
 const dogBtn = document.getElementById('btn-dog');
-// const petBtn = document.getElementsByClassName('petBtn');
 
-const check = (pet)=>{
-    if(!pet[0].classList.contains('hide')){
-        pet.forEach(element => {
-            element.classList.add('hide')
-        });
-    } 
+// Função para verificar se Cat ou Dog já tem a classe hide. Para apenas um dos dois serem mostrados de cada vez
+//Obs.: O primeiro console log foi adc pois não consigo deixar sem nada ali
+const hideDisplay = (pet) => {
+    (pet[0].classList.contains('hide')) ? console.log('Todos os pets são anjos') : pet.forEach(element => {element.classList.add('hide')})   
 }
 
-const petDisplay =(petDisplay, petHide)=>{
-    petDisplay.forEach(element => {
+// Função para percorrer a lista e remover o classe que a deixa escondida e invocando a função da linha 11 para fazer a verificação
+const petDisplay =(petShow, petHide)=>{
+    petShow.forEach(element => {
         element.classList.remove('hide')
     });
-    check(petHide)
+    hideDisplay(petHide)
 }
 
-
+// Adiciona o evento para invocar a função da linha 16 quando o user 'clicar' nos botoes capturados
+//Obs.: Foi criada uma função anônima para invocar a função pois não consegui add os parâmetros de outra forma
 catBtn.addEventListener("click", ()=>{petDisplay(cat, dog)})
 dogBtn.addEventListener("click", ()=>{petDisplay(dog, cat)})
-
-//Quando apertar o botao gato, add hide aos dogs
-
-// const check = ()=>{
-//     if(!dog[0].classList.contains('hide')){
-//         dogDisplay()
-//     } 
-// }
-// const check2 = ()=>{
-//     if(!cat[0].classList.contains('hide')){
-//         catDisplay()
-//     } 
-// }
-
-// const catDisplay =()=>{
-//         cat.forEach(element => {
-//             element.classList.toggle('hide')
-//         });
-//         check()
-//         // dogDisplay()
-//     }
-
-// const dogDisplay =()=> {
-//     dog.forEach(element => {
-//         element.classList.toggle('hide')
-//     });
-//     check2()
-// }
-
-
-
-
-// catBtn.addEventListener("click", catDisplay)
-// dogBtn.addEventListener("click", dogDisplay)
